7cd483db6a2227e089e0ad101c2ae721192ebd69
Marcelo Paez Sequeira <marcelo@blastradius.ai>
2022-09-19 17:42:49 -0300

    Fixed nvim icons

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -2,6 +2,7 @@ return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
 
   use 'kyazdani42/nvim-tree.lua'
+  use 'kyazdani42/nvim-web-devicons'
 
   use {
     'yamatsum/nvim-nonicons',
